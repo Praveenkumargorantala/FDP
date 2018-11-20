1640b2b125ce16aca3233875e49a38582634386a
Praveen_Kumar_G <Praveen_Kumar_G@EPINHYDL0065.princeton.epam.com>
2018-11-20 11:15:38 +0530

    2nd commit

--- FDP/src/com/epam/ConversionProblem.java
+++ FDP/src/com/epam/ConversionProblem.java
@@ -2,4 +2,15 @@ package com.epam;
 
 public class ConversionProblem {
 
+	float ConversionToInches(int centimeter) 
+    { 
+        float inches = 0.3937f * centimeter;   
+        return inches;
+    } 
+
+	float ConversionToFeets(int centimeter) 
+    {  
+        float feet = 0.0328f * centimeter;  
+        return feet;
+    } 
 }
