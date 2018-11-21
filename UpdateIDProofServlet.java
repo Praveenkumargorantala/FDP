400e0cf0f99e8394d003529f09168d3761af8e19
Praveenkumargorantala <45034875+Praveenkumargorantala@users.noreply.github.com>
2018-11-21 16:09:12 +0530

    Update UpdateIDProofServlet.java

--- UpdateIDProofServlet.java
+++ UpdateIDProofServlet.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,7 @@ import com.epam.dao.IDProofDAO;
 /**
  * Servlet implementation class UpdateIDProofServlet
  */
+@MultipartConfig
 public class UpdateIDProofServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -32,6 +34,7 @@ public class UpdateIDProofServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("in doGet()");
 		doPost(request, response);
 }
 
@@ -40,16 +43,21 @@ public class UpdateIDProofServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		System.out.println("in doPost()");
 		response.setContentType("text/html"); 
 	    PrintWriter out = response.getWriter();
 	    
 	    String emailID = request.getParameter("emailID"); 
-		  String typeOfIDProof = request.getParameter("typeOfIDProof");
+		  String typeOfIDProof = request.getParameter("typeOfIdProof");
 		  
+		  System.out.println("emailID="+emailID);
+		  System.out.println("typeOfIDProof="+typeOfIDProof);
+		  System.out.println("part="+ request.getPart("idProof"));
 		  InputStream inputStream = null; // input stream of the upload file
 	         
 	        // obtains the upload file part in this multipart request
-	        Part filePart = request.getPart("photo");
+	        Part filePart = request.getPart("idProof");
+	        System.out.println(filePart);
 	        if (filePart != null) {
 	            // prints out some information for debugging
 	            System.out.println(filePart.getName());
@@ -59,14 +67,18 @@ public class UpdateIDProofServlet extends HttpServlet {
 	            // obtains input stream of the upload file
 	            inputStream = filePart.getInputStream();
 	            
+	            System.out.println("in UpdateIDProofServlet: before create object of IDProofBean");
 	            IDProofBean idProofBean = new IDProofBean(emailID,typeOfIDProof,filePart);
 	            IDProofDAO  idProofDAO= new IDProofDAOImpl();
 	            
 	            if(idProofDAO.updateIDProof(idProofBean)) {
-	                out.println("IDProof has been successfully updated"); 
+	                //out.println("IDProof has been successfully updated");
+	            	response.sendRedirect("UpdateSucess.jsp");
 	            } 
 	            else { 
-	                out.println("IDProof could not be updated");
+	                //out.println("IDProof could not be updated");
+	            	response.sendRedirect("UpdateFailure.jsp");
+	                
 	            }
 	        }
 	}
